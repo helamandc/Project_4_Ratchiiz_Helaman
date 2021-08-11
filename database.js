72fb64835aadd5221a1e46361763f8811aff482e
maneerat maneerat <maneeratpsw@gmail.com>
2021-08-12 01:03:54 +1000

    Lasted add CSS part

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const pgp = require('pg-promise')()
 
 const username = process.env.PG_USER || 'postgres'
-const password = process.env.PG_PASSWORD 
+const password = process.env.PG_PASSWORD || '01201989hvd'
 const host = process.env.PG_HOST || 'localhost'
 const port = process.env.PG_PORT || '5432'
 const database = 'mrcoffee'
